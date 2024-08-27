6cee845e7ad038b3ba7d0bcea1afac51860971a9
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-27 21:09:32 +0200

    Add Header component
    - Created Header component with a link to the homepage
    - Used Next.js Link component for client-side navigation
    - Adding 2 main colors to the tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#5542F6",
+        secondary: "#222222",
       },
     },
   },

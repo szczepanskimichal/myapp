31c01e1266ecad22c2bd6dc067fbc89cc818ab7c
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-27 22:28:43 +0200

    Enhance Header component with dynamic link styling
    - Create UserIcon & ShoppingCardIcon
    - Added dynamic styling for active and inactive links
    - Utilized Next.js useRouter to determine the current path
    - Applied conditional classes to highlight the active link
    - Included UserIcon and ShoppingCartIcon components

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import UserIcon from "./icons/UserIcon";
+import ShoppingCartIcon from "./icons/ShoppingCardIcon";
 
 export default function Header() {
   const inactiveLink =
@@ -36,6 +38,22 @@ export default function Header() {
           <div>
             <div>Categories</div>
           </div>
+        </nav>
+        <nav className="flex gap-10 items-center">
+          <Link
+            href={"/account/profile"}
+            className="transition delay-150 duration-300 hover:text-primary"
+          >
+            <UserIcon className="size-7" />
+          </Link>
+          <Link href={"/cart"} className="group">
+            <div className="flex items-center h-[60px] relative transition delay-150 duration-300 group-hover:text-primary">
+              <ShoppingCartIcon />
+              <div className="absolute top-2 left-4 bg-secondary text-white border-2 border-white rounded-full items-center justify-center flex size-5 text-xs transition delay-150 duration-300 group-hover_text-primary group-hover:border-primary">
+                0
+              </div>
+            </div>
+          </Link>
         </nav>
       </header>
     </>

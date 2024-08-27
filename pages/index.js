85eff7fbeb6988263d70df398bd532ccea1615a2
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-28 00:33:58 +0200

    Implement Server-Side Rendering for Featured Product
    - Added `getServerSideProps` function to fetch a featured product by its ID from the database using Mongoose.
    - Connected to the MongoDB database with `mongooseConnect()` to retrieve the featured product details.
    - Serialized the product data using `JSON.stringify` to ensure compatibility when passing the data as props.
    - This setup allows for dynamic server-side rendering of the featured product on page load.
    - Folder modes & lib are copyied from admin

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Header from "../components/Header";
 import Featured from "../components/Featured";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
 export default function Home() {
   return (
@@ -10,4 +12,15 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = "asdfasd";
+  await mongooseConnect();
+  const featuredProduct = await Product.findById(featuredProductId);
+  return {
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+    },
+  };
 }

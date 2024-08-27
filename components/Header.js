854fdd714da93e5fb0ad212472e9db29fb5013c1
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-27 23:45:21 +0200

    MobileMenu is working fine :)
    Correct mobile menu toggle and CSS class issues in Header component
    
    - Fixed incorrect CSS class `group-hover_text-primary` to `group-hover:text-primary`.
    - Corrected the function to close the mobile menu from `setNavOpen(false)` to `setIsMobileMenuOpen(false)`.
    
    This ensures the mobile menu opens and closes correctly and the hover effects are applied properly

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+import { slideIn } from "@/utils/motion";
 import UserIcon from "./icons/UserIcon";
 import ShoppingCartIcon from "./icons/ShoppingCardIcon";
 import Hamburger from "./icons/Hamburger";
+import CloseIcon from "./icons/CloseIcon";
 
 export default function Header() {
   const inactiveLink =
@@ -53,7 +57,7 @@ export default function Header() {
           <Link href={"/cart"} className="group">
             <div className="flex items-center h-[60px] relative transition delay-150 duration-300 group-hover:text-primary">
               <ShoppingCartIcon />
-              <div className="absolute top-2 left-4 bg-secondary text-white border-2 border-white rounded-full items-center justify-center flex size-5 text-xs transition delay-150 duration-300 group-hover_text-primary group-hover:border-primary">
+              <div className="absolute top-2 left-4 bg-secondary text-white border-2 border-white rounded-full items-center justify-center flex size-5 text-xs transition delay-150 duration-300 group-hover:text-primary group-hover:border-primary">
                 0
               </div>
             </div>
@@ -61,7 +65,9 @@ export default function Header() {
         </nav>
       </header>
       <header
-        className={`w-full sm:hidden flex justify-around h-[70px] items-center transition duration-300 delay-150 `}
+        className={`w-full sm:hidden flex justify-around h-[70px] items-center transition duration-300 delay-150 text-white ${
+          isMobileMenuOpen ? "bg-black" : "bg-secondary"
+        }`}
       >
         <div onClick={() => setIsMobileMenuOpen(true)}>
           <Hamburger />
@@ -70,11 +76,51 @@ export default function Header() {
         <Link href={"/cart"} className="group">
           <div className="flex items-center h-[60px] relative transition delay-150 duration-300 group-hover:text-primary">
             <ShoppingCartIcon />
-            <div className="absolute top-2 left-4 bg-secondary text-white border-2 border-white rounded-full items-center justify-center flex size-5 text-xs transition delay-150 duration-300 group-hover_text-primary group-hover:border-primary">
+            <div className="absolute top-2 left-4 bg-secondary text-white border-2 border-white rounded-full items-center justify-center flex size-5 text-xs transition delay-150 duration-300 group-hover:text-primary group-hover:border-primary">
               0
             </div>
           </div>
         </Link>
+        <AnimatePresence>
+          {isMobileMenuOpen && (
+            <motion.nav
+              variants={slideIn("left", "tween", 0, 0.5, false)}
+              initial="hidden"
+              whileInView="show"
+              exit="exit"
+              className="absolute top-0 left-0 h-screen bg-secondary w-[60%] pl-[60px]"
+            >
+              <div
+                onClick={() => setIsMobileMenuOpen(false)}
+                className="absolute top-5 right-5"
+              >
+                <CloseIcon />
+              </div>
+              <div className="flex flex-col justify-between h-[40%]">
+                <nav className="flex flex-col gap-10 mt-[100px] justify-center">
+                  <Link href={"/"} className={inactiveLink}>
+                    Home
+                  </Link>
+                  <Link href={"/products"} className={inactiveLink}>
+                    All Products
+                  </Link>
+                  <Link href={"/categories"} className={inactiveLink}>
+                    Categories
+                  </Link>
+                </nav>
+                <nav className="flex flex-col gap-10 justify-center">
+                  <Link
+                    href={"/account/profile"}
+                    className="flex gap-3 items-center hover:text-primary"
+                  >
+                    Account
+                    <UserIcon className="size-7" />
+                  </Link>
+                </nav>
+              </div>
+            </motion.nav>
+          )}
+        </AnimatePresence>
       </header>
     </>
   );

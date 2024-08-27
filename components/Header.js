61671b5ac23d07a3414e67e7675f4a5e620c92cf
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-27 21:51:14 +0200

    Implement dynamic link styling in Header component
    - Added dynamic styling for active and inactive links
    - Utilized Next.js useRouter to determine the current path
    - Applied conditional classes to highlight the active link

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,38 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const inactiveLink =
+    "hover:text-primary hover:scale-105 hover:decoration-primary decoration-secondary underline underline-offset-4 transition-all duration-300 delay-150";
+  const activeLink = inactiveLink.replace(
+    "decoration-secondary",
+    "decoration-white"
+  );
+
+  const router = useRouter();
+  const { pathname } = router;
+
   return (
     <>
       <header className="fixed top-0 w-full hidden sm:flex justify-around h-[80px] items-center bg-secondary text-white z-2">
-        <Link href="/">Ecommerce</Link>
+        <Link href="/" className={inactiveLink}>
+          Ecommerce
+        </Link>
         <nav className="flex gap-10">
-          <Link href={"/"}>Home</Link>
-          <Link href={"/products"}>All Products</Link>
+          <Link
+            href={"/"}
+            className={`${pathname === "/" ? activeLink : inactiveLink}`}
+          >
+            Home
+          </Link>
+          <Link
+            href={"/products"}
+            className={`${
+              pathname.includes("/products") ? activeLink : inactiveLink
+            }`}
+          >
+            All Products
+          </Link>
           <div>
             <div>Categories</div>
           </div>

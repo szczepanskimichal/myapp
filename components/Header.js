0912351899a0ee0d00df86e3f194009cd61f1ce0
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-27 22:52:18 +0200

    Enhance Header component with dynamic link styling and icons
    - Added dynamic styling for active and inactive links
    - Utilized Next.js useRouter to determine the current path
    - Applied conditional classes to highlight the active link
    - Included UserIcon and ShoppingCartIcon components
    - Implemented mobile menu state management

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import UserIcon from "./icons/UserIcon";
 import ShoppingCartIcon from "./icons/ShoppingCardIcon";
+import Hamburger from "./icons/Hamburger";
 
 export default function Header() {
   const inactiveLink =
@@ -13,6 +15,8 @@ export default function Header() {
 
   const router = useRouter();
   const { pathname } = router;
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
     <>
@@ -56,7 +60,22 @@ export default function Header() {
           </Link>
         </nav>
       </header>
-      <header></header>
+      <header
+        className={`w-full sm:hidden flex justify-around h-[70px] items-center transition duration-300 delay-150 `}
+      >
+        <div onClick={() => setIsMobileMenuOpen(true)}>
+          <Hamburger />
+        </div>
+        <Link href={"/"}>Ecommerce</Link>
+        <Link href={"/cart"} className="group">
+          <div className="flex items-center h-[60px] relative transition delay-150 duration-300 group-hover:text-primary">
+            <ShoppingCartIcon />
+            <div className="absolute top-2 left-4 bg-secondary text-white border-2 border-white rounded-full items-center justify-center flex size-5 text-xs transition delay-150 duration-300 group-hover_text-primary group-hover:border-primary">
+              0
+            </div>
+          </div>
+        </Link>
+      </header>
     </>
   );
 }

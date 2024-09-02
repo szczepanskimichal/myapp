8c7402f9c8ac3dc6c89796864efb18945f0655cf
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-02 15:06:45 +0200

    category filters are displayed and on the product card

--- pages/categories/[id].js
+++ pages/categories/[id].js
@@ -1,9 +1,16 @@
 import Layout from "@/components/Layout";
+import { Product } from "@/models/Product";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/Category";
 import { FaArrowLeft } from "react-icons/fa";
 
-export default function CategoryPage({ category, parent }) {
+export default function CategoryPage({
+  category,
+  products,
+  parent,
+  properties,
+  productIds,
+}) {
   return (
     <Layout>
       <div className="flex flex-col items-center gap-10 p-5">
@@ -17,6 +24,18 @@ export default function CategoryPage({ category, parent }) {
               </span>
             )}
           </div>
+          <div className="flex gap-5">
+            {properties.map((property) => (
+              <div
+                key={property.name}
+                className="bg-white p-2 rounded-md flex gap-2 items-center shadow-md"
+              >
+                <div className="whitespace-nowrap capitalize">
+                  {property.name}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
@@ -26,15 +45,44 @@ export default function CategoryPage({ category, parent }) {
 export async function getServerSideProps(context) {
   const { id } = context.query;
   await mongooseConnect();
+
   const category = await Category.findById(id);
-  const parent = category?.parent
-    ? await Category.findById(category.parent)
-    : null;
+  const parentId = category?.parent;
+  const parent = await Category.findById(parentId);
+  const categories = await Category.find();
+
+  const childCategoryIds = categories.filter(
+    (category) => category.parent?._id.toString() === id
+  );
+
+  const categoryProducts = await Product.find({ category: id }).sort({
+    id: -1,
+  });
+
+  const childProductsPromises = childCategoryIds.map((childId) =>
+    Product.find({ category: childId }).sort({ id: -1 })
+  );
+  const childProducts = await Promise.all(childProductsPromises);
+
+  const products = categoryProducts.concat(...childProducts);
+  const productIds = products.map((product) => product.id);
+
+  let properties = [];
+
+  if (category?.properties) {
+    properties.push(...category.properties);
+  }
+  if (parent?.properties && !Object.keys(category.properties).length) {
+    properties.push(...parent.properties);
+  }
 
   return {
     props: {
       category: JSON.parse(JSON.stringify(category)),
       parent: JSON.parse(JSON.stringify(parent)),
+      properties: JSON.parse(JSON.stringify(properties)),
+      products: JSON.parse(JSON.stringify(products)),
+      productIds: JSON.parse(JSON.stringify(productIds)),
     },
   };
 }

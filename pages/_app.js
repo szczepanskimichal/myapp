53522c3372f6e5c0eeba2d98ec922019efab419a
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-03 09:25:10 +0200

    Initialize global state and toast notifications in App component
    - Wrapped the application with `CartContextProvider` to manage global cart state.
    - Integrated `react-hot-toast` Toaster for global toast notifications.
    - Configured toast notifications with custom styling, positioning, and behavior.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
+import { CartContextProvider } from "@/components/CartContext";
+import { Toaster } from "react-hot-toast";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CartContextProvider>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8} // defines the spacing between toasts in pixels.
+        toastOptions={{
+          duration: 3000,
+          style: {
+            fontSize: "20px",
+            padding: "8px 16px",
+            display: "inline-flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            whiteSpace: "nowrap",
+          },
+        }}
+      />
+      <Component {...pageProps} />
+    </CartContextProvider>
+  );
 }

ef8a3c1b4a3fdfbc05bf1c122fcea1c515db5d0f
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-10 21:26:32 +0200

    Fix typos

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,10 +1,12 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/Category";
 
-export default async function handler(req, res) {
+export default async function handle(req, res) {
   await mongooseConnect();
 
   if (req.method === "GET") {
+    const categories = await Category.find();
+    categories.sort((a, b) => a.name.localeCompare(b.name));
     res.json(await Category.find());
   }
 }

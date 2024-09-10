7c02df43d84773f880dce18bc26ac73a361fd204
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-10 23:13:35 +0200

    Added NextAuth SessionProvider and refactored App component
    - Wrapped the App component with SessionProvider to enable authentication session management
    - Maintained CartContextProvider for cart functionality
    - Configured the Toaster for consistent toast notifications
    - Ensured smooth integration of session management and cart context in the global app

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,30 @@
 import { CartContextProvider } from "@/components/CartContext";
 import { Toaster } from "react-hot-toast";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <CartContextProvider>
-      <Toaster
-        position="top-center"
-        reverseOrder={false} //keeps the order of the toasts as they are triggered.
-        gutter={8} // defines the spacing between toasts in pixels.
-        toastOptions={{
-          duration: 3000,
-          style: {
-            fontSize: "20px",
-            padding: "8px 16px",
-            display: "inline-flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            whiteSpace: "nowrap",
-          },
-        }}
-      />
-      <Component {...pageProps} />
-    </CartContextProvider>
+    <SessionProvider>
+      <CartContextProvider>
+        <Toaster
+          position="top-center"
+          reverseOrder={false} //keeps the order of the toasts as they are triggered.
+          gutter={8} // defines the spacing between toasts in pixels.
+          toastOptions={{
+            duration: 3000,
+            style: {
+              fontSize: "20px",
+              padding: "8px 16px",
+              display: "inline-flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              whiteSpace: "nowrap",
+            },
+          }}
+        />
+        <Component {...pageProps} />
+      </CartContextProvider>
+    </SessionProvider>
   );
 }

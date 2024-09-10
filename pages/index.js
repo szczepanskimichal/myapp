4f5d27fef4a4bc56a49011de9905d0f2b386d1ba
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-09-10 21:30:58 +0200

    Refactor Home component and improve data fetching in getServerSideProps.
    - Modified `getServerSideProps` to improve product and category fetching logic:
      - Added sorting of categories by name for better presentation.
      - Combined limit and sorting logic when fetching new products for clarity and performance.
    - Ensured consistent JSON parsing of fetched data for server-side rendering.
    - Minor restructuring of the `Home` component for cleaner organization of JSX elements.

--- pages/index.js
+++ pages/index.js
@@ -21,13 +21,17 @@ export async function getServerSideProps() {
   const featuredProductId = "66964e512c84a13a661c4eb6";
   await mongooseConnect();
   const featuredProduct = await Product.findById(featuredProductId);
-  const newProducts = await Product.find().sort({ createdAt: -1 }).limit(10);
-  const categories = await Category.find({}, null, { sort: { _id: -1 } });
-
+  const newProducts = await Product.find({}, null, {
+    sort: { _id: -1 },
+    limit: 10,
+  });
+  const categories = await Category.find();
+  categories.sort((a, b) => a.name.localeCompare(b.name));
   return {
     props: {
       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
       newProducts: JSON.parse(JSON.stringify(newProducts)),
+      categories: JSON.parse(JSON.stringify(categories)),
     },
   };
 }

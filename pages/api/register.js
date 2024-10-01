ab4bf65895824d45625c1be57c22884774d97172
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-10-01 22:28:44 +0200

    w

--- pages/api/register.js
+++ pages/api/register.js
@@ -3,34 +3,34 @@ import { User } from "@/models/User";
 import bcrypt from "bcrypt";
 
 export default async function handle(req, res) {
-	if (req.method === "POST") {
-		await mongooseConnect();
+  if (req.method === "POST") {
+    await mongooseConnect();
 
-		const { email, password } = req.body;
+    const { email, password } = req.body;
 
-		if (!password || password.length < 8) {
-			res
-				.status(405)
-				.json({ error: "Password must have at least 8 characters." });
-		}
+    if (!password || password.length < 8) {
+      res
+        .status(405)
+        .json({ error: "Password must have at least 8 characters." });
+    }
 
-		const salt = bcrypt.genSaltSync(10);
-		const hashedPassword = bcrypt.hashSync(password, salt);
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
-		try {
-			const user = await User.create({
-				email,
-				password: hashedPassword,
-			});
-			res.json(user);
-		} catch (error) {
-			if (error.code === 11000 && error.keyValue.email) {
-				res.status(409).json({ error: "Email address already exists" });
-			} else {
-				console.error("Error creating user:", error);
-				res.status(500).json({ error: "Unexpected error occured" });
-			}
-		}
-		res.json(true);
-	}
+    try {
+      const user = await User.create({
+        email,
+        password: hashedPassword,
+      });
+      return res.json(user);
+    } catch (error) {
+      if (error.code === 11000 && error.keyValue.email) {
+        res.status(409).json({ error: "Email address already exists" });
+      } else {
+        console.error("Error creating user:", error);
+        res.status(500).json({ error: "Unexpected error occured" });
+      }
+    }
+    res.json(true);
+  }
 }
